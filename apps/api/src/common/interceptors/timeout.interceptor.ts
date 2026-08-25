04bd51fabc792753fe343b493c0c2ae128416815
sachinthacham <sachinthachamindu26@gmail.com>
2026-08-25 23:26:44 +0530

    feat: add request timeout interceptor

---
+++ apps/api/src/common/interceptors/timeout.interceptor.ts
@@ -0,0 +1,35 @@
+import {
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+  RequestTimeoutException,
+} from '@nestjs/common';
+import { Observable, throwError, TimeoutError } from 'rxjs';
+import { catchError, timeout } from 'rxjs/operators';
+
+@Injectable()
+export class TimeoutInterceptor implements NestInterceptor {
+  private readonly timeoutMs: number;
+
+  constructor(timeoutMs = 30000) {
+    this.timeoutMs = timeoutMs;
+  }
+
+  intercept(
+    _context: ExecutionContext,
+    next: CallHandler,
+  ): Observable<unknown> {
+    return next.handle().pipe(
+      timeout(this.timeoutMs),
+      catchError((err: unknown) => {
+        if (err instanceof TimeoutError) {
+          return throwError(
+            () => new RequestTimeoutException('Request timed out'),
+          );
+        }
+        return throwError(() => err as Error);
+      }),
+    );
+  }
+}
